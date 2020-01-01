b85bd18a59b66ba874b87c9c7fee783c02859ed0
Vincent-Carrier <vincentcarrier15@gmail.com>
2020-01-01 11:22:30 -0500

    [WIP] Iterate ALL THE THINGS

--- core/src/slide.rs
+++ core/src/slide.rs
@@ -1,138 +1,92 @@
-use std::ops;
-use crate::sq::*;
+use crate::sq::{Vec2, Sq, SqSize, four_directions};
 use crate::color::*;
 use crate::board::*;
 use crate::piece::*;
 use crate::piece::PieceKind::*;
 
-
-impl ops::Add<Vec2> for Vec2 {
-  type Output = Vec2;
-  fn add(self, _rhs: Vec2) -> Vec2 {
-    Vec2 { x: self.x + _rhs.x, y: self.y + _rhs.y }
-  }
+lazy_static! {
+  static ref DIAGONALS: Vec<Vec2> =
+    four_directions(Vec2 { x: 1, y: 1 }).collect();
+ static ref STRAIGHT_LINES: Vec<Vec2> =
+    four_directions(Vec2 { x: 0, y: 1 }).collect();
+  static ref EIGHT_DIRECTIONS: Vec<Vec2> =
+    four_directions(Vec2 { x: 1, y: 1 }).chain(four_directions(Vec2 { x: 0, y: 1 })).collect();
+  static ref KNIGHT_DIRECTIONS: Vec<Vec2> =
+    four_directions(Vec2 { x: 1, y: 2 }).chain(four_directions(Vec2 { x: 2, y: 1 })).collect();
 }
-
-// static BOUNDS: Range = Range { start: 0, end: 8 };
-
-impl Vec2 {
-  fn scale(self, n: SqSize) -> Vec2 {
-    Vec2 { x: self.x * n, y: self.y * n }
-  }
-
-  const fn rotate(self) -> Vec2 {
-    Vec2 { x: -self.y, y: self.x }
-  }
-
-  fn inside_board(self) -> bool {
-    0 <= self.x && self.x <= 7 &&
-    0 <= self.y && self.y <= 7
-  }
-}
-
-// Rust's lack of compile-time loops is infuriating
-// TODO: Fix with lazy-static
-const fn four_directions(dir: Vec2) -> [Vec2; 4] {
-  [ dir, dir.rotate(), dir.rotate().rotate(), dir.rotate().rotate().rotate() ]
-}
-
-const fn eight_directions(dir: Vec2) -> [Vec2; 8] {
-  let alt = Vec2 { x: 0, y: dir.y };
-  [ dir, dir.rotate(), dir.rotate().rotate(), dir.rotate().rotate().rotate() ,
-    alt, alt.rotate(), alt.rotate().rotate(), alt.rotate().rotate().rotate() ]
-}
-
-const fn knight_directions(dir: Vec2) -> [Vec2; 8] {
-  let inv = Vec2 { x: dir.y, y: dir.x };
-  [ dir, dir.rotate(), dir.rotate().rotate(), dir.rotate().rotate().rotate() ,
-    inv, inv.rotate(), inv.rotate().rotate(), inv.rotate().rotate().rotate() ]
-}
-
-static DIAGONALS        : [Vec2; 4] = four_directions(Vec2 { x: 1, y: 1 });
-static STRAIGHT_LINES   : [Vec2; 4] = four_directions(Vec2 { x: 0, y: 1 });
-static KNIGHT_DIRECTIONS: [Vec2; 8] = knight_directions(Vec2 { x: 1, y: 2 });
-static EIGHT_DIRECTIONS : [Vec2; 8] = eight_directions(Vec2 { x: 1, y: 1 });
 
 #[derive(PartialEq)]
 enum CaptureStyle {
   Can, Cannot, Must
 }
 
-use crate::slide::CaptureStyle::*;
+use self::CaptureStyle::*;
 
-fn slide(from: Sq, towards: Vec2, color: Color, board: &Board,
-         max: SqSize, capture_style: CaptureStyle) -> Vec<Sq> {
-  let mut sqs: Vec<Sq> = Vec::new();
-  let mut i = 1;
-  loop {
-    let candidate = from + towards.scale(i);
-    if candidate.inside_board() && i <= max {
-      match board[candidate] {
-        None if capture_style == Must => return sqs,
-        None => sqs.push(candidate),
-        Some(p) if p.color == color || capture_style == Cannot => return sqs,
-        Some(_) => { sqs.push(candidate); return sqs },
-      }
-    } else {
-      return sqs
-    }
-    i += 1;
-  }
+struct SlideIter<'a> {
+  board: &'a Board,
+  piece: Piece
+  from: Sq,
+  dir: Vec2,
+  capture: CaptureStyle,
+  i: SqSize,
+  max: SqSize
 }
 
+impl Iterator for SlideIter<'_> {
+  type Item = Sq;
+
+  fn next(&mut self) -> Option<Sq> {
+    if self.i >= self.max { return None }
+    self.i += 1;
+    let sq = self.from + (self.dir * self.i);
+    if !sq.inside_board() { return None };
+    match self.board[sq] {
+      None if self.capture != Must => Some(sq),
+      Some(p) if p.color == self.piece.color => { self.i = self.max; Some(sq) },
+      Some(p) if p.color != self.piece.color && self.capture != Cannot => Some(sq),
+      _ => None
+    }
+  }
+}
 
 impl Piece {
   pub fn moves(&self, from: Sq, board: &Board) -> Vec<Sq> {
-    match &self.kind {
-      Pawn   => {
+    let slide = |dir, max, capture| {
+      SlideIter { board, from, dir, piece: *self, capture, i: 0, max }
+    };
+
+    if self.kind == Pawn {
         let forward_dist = if from.y == self.color.pawn_row() { 2 } else { 1 };
         let forward_dir = Vec2 { x: 0, y: self.color.forward() };
-        let capture_dirs = vec![
+        let capture_dirs = [
           Vec2 { x: -1, y: self.color.forward() },
           Vec2 { x: 1, y: self.color.forward() },
         ];
-        let forward_moves = slide(from, forward_dir, self.color, board, forward_dist, Cannot);
-        let capture_moves: Vec<Vec2> = capture_dirs.iter().flat_map(
-                                       |&dir| slide(from, dir, self.color, board, 1, Must)).collect();
-        [&forward_moves[..], &capture_moves[..]].concat()
-      }
-      Knight => KNIGHT_DIRECTIONS.iter().flat_map(
-                |&dir| slide(from, dir, self.color, board, 1, Can)).collect(),
-      Bishop => DIAGONALS.iter().flat_map(
-                |&dir| slide(from, dir, self.color, board, 8, Can)).collect(),
-      Rook   => STRAIGHT_LINES.iter().flat_map(
-                |&dir| slide(from, dir, self.color, board, 8, Can)).collect(),
-      Queen  => EIGHT_DIRECTIONS.iter().flat_map(
-                |&dir| slide(from, dir, self.color, board, 8, Can)).collect(),
-      King   => EIGHT_DIRECTIONS.iter().flat_map(
-                |&dir| slide(from, dir, self.color, board, 1, Can)).collect(),
+
+        return capture_dirs.iter().flat_map(|dir| { slide(*dir, 1, Must) }).chain(
+          slide(forward_dir, forward_dist, Cannot)
+        ).collect()
     }
-  }
 
-  pub iter(&self) -> Iter {
-    Iter {
-      piece: self,
+    let dirs = match self.kind {
+      Knight       => *KNIGHT_DIRECTIONS,
+      Bishop       => *DIAGONALS,
+      Rook         => *STRAIGHT_LINES,
+      King | Queen => *EIGHT_DIRECTIONS,
+    };
 
-    }
-  }
-}
-
-struct Iter<'a> {
-  piece: &'a Piece
-  dirs: &'static [Vec2],
-  dir_i: usize,
-  i: SqSize,
-  capturing: CaptureStyle,
-
-}
-
-impl Iterator for Iter<'_> {
-  type Item = Sq;
-
-  fn next(&mut self) -> Option<Self::Item> {
-
+    dirs.iter().flat_map(
+      |dir| {
+          let max = match self.kind {
+            Knight | King => 1,
+            _ => 8
+          };
+          slide(*dir, max, Can)
+        }
+    ).collect()
   }
 }
+
 
 pub fn display_moves(moves: &[Vec2]) {
   for y in 0..8 {
--- core/src/sq.rs
+++ core/src/sq.rs
@@ -7,3 +7,49 @@ pub struct Vec2 {
 
 pub type Sq = Vec2;
 
+
+impl std::ops::Add<Vec2> for Vec2 {
+  type Output = Vec2;
+  fn add(self, rhs: Vec2) -> Vec2 {
+    Vec2 { x: self.x + rhs.x, y: self.y + rhs.y }
+  }
+}
+
+impl std::ops::Mul<SqSize> for Vec2 {
+  type Output = Vec2;
+  fn mul(self, rhs: SqSize) -> Vec2 {
+    Vec2 { x: self.x * rhs, y: self.y * rhs }
+  }
+}
+
+impl Vec2 {
+  fn scale(self, n: SqSize) -> Vec2 {
+    Vec2 { x: self.x * n, y: self.y * n }
+  }
+
+  const fn rotate(self) -> Vec2 {
+    Vec2 { x: -self.y, y: self.x }
+  }
+
+  pub fn inside_board(self) -> bool {
+    0 <= self.x && self.x <= 7 &&
+    0 <= self.y && self.y <= 7
+  }
+}
+
+struct RotationIter {
+  dir: Vec2
+}
+
+impl Iterator for RotationIter {
+  type Item = Vec2;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    self.dir = self.dir.rotate();
+    Some(self.dir)
+  }
+}
+
+pub fn four_directions(dir: Vec2) -> std::iter::Take<RotationIter> {
+  RotationIter { dir }.take(4)
+}
