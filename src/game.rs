71e645f8fd84c3ea35a3240a67f76546226087b3
Vincent-Carrier <vincentcarrier15@gmail.com>
2020-01-06 13:05:53 -0500

    Add GameBuilder and observe option

--- src/game.rs
+++ src/game.rs
@@ -1,3 +1,8 @@
+use std::thread;
+use std::time::Duration;
+use uci::engine::UciEngine;
+use crate::cli::CLI;
+use engine::engine::Engine;
 use base::prelude::Color::{White,Black};
 use std::collections::BTreeMap;
 use base::player::Player;
@@ -6,17 +11,11 @@ use base::prelude::KingState::{Safe, Check};
 
 pub struct Game {
   state: State,
-  players: BTreeMap<Color, Box<dyn Player>>
+  players: BTreeMap<Color, Box<dyn Player>>,
+  observe: bool,
 }
 
 impl Game {
-  pub fn new(player1: Box<dyn Player>, player2: Box<dyn Player>) -> Game {
-    let mut players = BTreeMap::new();
-    players.insert(White, player1);
-    players.insert(Black, player2);
-    Game { state: State::new(), players }
-  }
-
   pub fn start(&mut self) {
     loop {
       match self.state.king_state {
@@ -27,10 +26,62 @@ impl Game {
           player.prompt_turn(&self.state);
           let mv = player.post_move(&self.state);
           other_player.accept_move(&mv, &self.state);
+
+          if self.observe {
+            println!("{}", self.state.board);
+            thread::sleep(Duration::from_millis(200))
+          }
           self.state.execute(mv);
         },
         _ => { panic!("Game over") }
       }
     }
+  }
+}
+
+pub struct GameBuilder {
+  player1: String,
+  player2: String,
+  observe: bool,
+}
+
+impl GameBuilder {
+  pub fn new() -> GameBuilder {
+    GameBuilder {
+      player1: "cli".to_owned(),
+      player2: "engine".to_owned(),
+      observe: false
+    }
+  }
+
+  pub fn player1(&mut self, player_type: String) -> &mut GameBuilder {
+    self.player1 = player_type;
+    self
+  }
+
+  pub fn player2(&mut self, player_type: String) -> &mut GameBuilder {
+    self.player2 = player_type;
+    self
+  }
+
+  pub fn observe(&mut self, observe: bool) -> &mut GameBuilder {
+    self.observe = observe;
+    self
+  }
+
+  pub fn build(&self) -> Game {
+    let mut players = BTreeMap::new();
+    players.insert(White, parse_player_type(&self.player1));
+    players.insert(Black, parse_player_type(&self.player2));
+    Game { state: State::new(), players, observe: self.observe }
+  }
+}
+
+fn parse_player_type(string: &str) -> Box<dyn Player> {
+  match string {
+    "cli" => Box::from(CLI {}),
+    "engine" => Box::from(Engine::new()),
+    "uci" => Box::from(UciEngine {}),
+    unknown => panic!(format!("Unexecpted player type {}", unknown))
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,15 @@ mod user_move;
 mod cli;
 mod game;
 
-use uci::engine::UciEngine;
-use crate::cli::CLI;
-use engine::engine::Engine;
-use base::player::Player;
-use crate::game::Game;
+use crate::game::GameBuilder;
 
-fn parse_player_type(string: &str) -> Box<dyn Player> {
-  match string {
-    "cli" => Box::from(CLI {}),
-    "engine" => Box::from(Engine::new()),
-    "uci" => Box::from(UciEngine {}),
-    unknown => panic!(format!("Unexecpted player type {}", unknown))
-  }
-}
 
 fn main() {
   let mut args = std::env::args().skip(1);
-  let player1 = parse_player_type(&args.next().unwrap_or("cli".to_string()));
-  let player2 = parse_player_type(&args.next().unwrap_or("engine".to_string()));
-  let mut game = Game::new(player1, player2);
+  let mut builder = GameBuilder::new();
+  if let Some(player1) = args.next() { builder.player1(player1); }
+  if let Some(player2) = args.next() { builder.player2(player2); }
+  if let Some(_) = args.next() { builder.observe(true); }
+  let mut game = builder.build();
   game.start();
 }
